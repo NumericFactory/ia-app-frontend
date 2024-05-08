255c490674e743b14c812ce45418e12dfdd8caed
NumericFactory <frederic.lossignol@gmail.com>
2024-05-08 16:33:21 +0200

    feat: show alert message when user click 'copy/paste' button

--- src/app/shared/services/alert.service.ts
+++ src/app/shared/services/alert.service.ts
@@ -8,7 +8,7 @@ export class AlertService {
 
   constructor(private snackbar: MatSnackBar) { }
 
-  show(message: string, type?: 'error' | 'success' | 'info') {
+  show(message: string, type?: 'error' | 'success' | 'info', duration?: number, position?: 'center' | 'right') {
     //if (!message) return;
     let cssclass = '';
     switch (type) {
@@ -17,9 +17,9 @@ export class AlertService {
       default: cssclass = 'info-snackbar'; // default value
     }
     this.snackbar.open(message, 'Fermer', {
-      duration: 6000,
+      duration: duration ? duration : 6000,
       verticalPosition: 'top',
-      horizontalPosition: 'right',
+      horizontalPosition: position ? position : 'right',
       panelClass: ['showbar', cssclass]
     })
   }
--- src/app/ui/public/ui-prompt-view/ui-conversation-ia/ui-conversation-ia.component.ts
+++ src/app/ui/public/ui-prompt-view/ui-conversation-ia/ui-conversation-ia.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, booleanAttribute } from '@angular/core';
 import { UiConversationLoaderSkeletonComponent } from '../ui-loader-skeleton/ui-conversation-loader-skeleton.component';
 import { DatePipe } from '@angular/common';
+import { AlertService } from '../../../../shared/services/alert.service';
 
 @Component({
   selector: 'ui-conversation-ia',
@@ -14,8 +15,11 @@ export class UiConversationIaComponent {
   @Input() responseAI: any; //{text: string, createdAt:string, }
   @Input({ transform: booleanAttribute }) isLoadingResponse = false;
 
+  constructor(private alertService: AlertService) { }
+
   copyToClipBoard(responseAIText: string) {
     navigator.clipboard.writeText(responseAIText);
+    this.alertService.show('Copié!', 'info', 2000, 'center');
 
   }
 
