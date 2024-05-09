c69b94d1a74d897ec1b8f2f10a5f514c87212cb0
NumericFactory <frederic.lossignol@gmail.com>
2024-05-09 19:21:20 +0200

    feat: button refresh IA Response - prompt view

--- src/app/ui/public/ui-prompt-view/ui-conversation-user/ui-conversation-user.component.ts
+++ src/app/ui/public/ui-prompt-view/ui-conversation-user/ui-conversation-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, booleanAttribute } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, Renderer2, SimpleChanges, ViewChild, booleanAttribute } from '@angular/core';
 import { PromptModel } from '../../../../core/models/step.model';
 import { ProgressBarModule } from 'primeng/progressbar';
 import { DatePipe } from '@angular/common';
@@ -13,6 +13,10 @@ import { UserModel } from '../../../../core/models/user.model';
 })
 export class UiConversationUserComponent {
 
+  // get htmlelement #refreshBtn
+  @ViewChild('refreshBtn') refreshBtn!: ElementRef;
+
+
   // role : affiche le prompt de l'utilisateur
   @Input() prompt!: PromptModel;
   @Input({ transform: booleanAttribute }) isLoadingResponse = false;
@@ -20,9 +24,25 @@ export class UiConversationUserComponent {
 
   @Output() refreshPromptEvent = new EventEmitter<PromptModel>();
 
+  constructor(private renderer: Renderer2) {
+
+  }
+
   // emit event to refresh prompt (parent prompt-view.component.ts)
   refreshPromptAction(prompt: PromptModel) {
+    console.log('UiConversationUserComponent', this.refreshBtn);
     this.refreshPromptEvent.emit(prompt);
+    this.renderer.setAttribute(this.refreshBtn.nativeElement, 'disabled', 'true');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('changes', changes);
+    if (changes['isLoadingResponse']
+      && changes['isLoadingResponse'].previousValue === true
+      && changes['isLoadingResponse'].currentValue === false) {
+      this.renderer.removeAttribute(this.refreshBtn.nativeElement, 'disabled');
+    }
+    //this.renderer.removeAttribute(this.refreshBtn.nativeElement, 'disabled');
   }
 
 
