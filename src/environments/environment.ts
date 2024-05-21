100f138443e3c282a27ba9287aaaec6a2a593f22
NumericFactory <frederic.lossignol@gmail.com>
2024-05-21 20:54:38 +0200

    change environmement to server Franck on Render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://ia-app-backend.onrender.com/api/v1'
+    // apiUrl: 'https://ia-app-backend.onrender.com/api/v1'
+    apiUrl: 'https://propulserai-backend.onrender.com/api/v1'
 };
